56636024817d4caa8d3074223ce6402317e9d72b
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-08-02 00:33:01 +0200

    Feature#7 add react router (#9)
    * :heavy_plus_sign: Add react-router
    
    * :heavy_plus_sign: Add react-router-dom
    
    * :sparkles: Setup router
    
    * :sparkles: react-scripts -> craco

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/* CSS */
 import './index.css';
 
+/* DEPENDANCIES */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<></>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
