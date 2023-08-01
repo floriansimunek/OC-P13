30c3e7a718ba184b5c8f14d2f4063e1d021672c5
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-08-02 01:24:56 +0200

    ✨ Feature#22 add signinform component (#24)
    * :sparkles: Add SignInForm component
    
    * :sparkles: Use SignInForm component in SignIn page component

---
+++ src/components/SignInForm/index.jsx
@@ -0,0 +1,28 @@
+/* CSS */
+import styles from './SignInForm.module.scss';
+
+export default function SignInForm() {
+    return (
+        <div className={styles.main}>
+            <section className={styles.SignInContent}>
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <form action="/user">
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                    </div>
+                    <div className={styles.inputRemember}>
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <button className={styles.SignInButton}>Sign In</button>
+                </form>
+            </section>
+        </div>
+    );
+}
