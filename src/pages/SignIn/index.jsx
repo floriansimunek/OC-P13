b6058aca3a14f8c31f5fda04c3662226a4513852
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-02 01:24:21 +0200

    :sparkles: Use SignInForm component in SignIn page component

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,10 @@
+/* COMPONENTS */
+import Form from '@components/SignInForm';
+
 export default function SignIn() {
-    return <main className="bgDark"></main>;
+    return (
+        <main className="bgDark">
+            <Form />
+        </main>
+    );
 }
