9bf61e21baa910e25bf3471ad11cce0c31d4a42d
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-02 01:41:24 +0200

    :sparkles: Use AccountsList component in User page component

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,10 +1,31 @@
 /* COMPONENTS */
 import UserHeader from '@components/UserHeader';
+import AccountsList from '@components/AccountsList';
+
+/* DATA */
+const ACCOUNTS = [
+    {
+        title: 'Argent Bank Checking (x8349)',
+        amount: 2082.79,
+        description: 'Available Balance',
+    },
+    {
+        title: 'Argent Bank Savings (x6712)',
+        amount: 10928.42,
+        description: 'Available Balance',
+    },
+    {
+        title: 'Argent Bank Credit Card (x8349)',
+        amount: 184.3,
+        description: 'Current Balance',
+    },
+];
 
 export default function User() {
     return (
         <main className="bgDark">
             <UserHeader />
+            <AccountsList accounts={ACCOUNTS} />
         </main>
     );
 }
