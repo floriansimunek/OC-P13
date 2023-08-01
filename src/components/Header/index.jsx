9aae067779400aa95b7bd7b0c81fe86c4ef6c196
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-02 00:41:56 +0200

    :sparkles: Add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,46 @@
+/* CSS */
+import styles from './Header.module.scss';
+
+/* DEPENDANCIES */
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+/* ASSETS */
+import logo from '@assets/argentBankLogo.png';
+
+export default function Header() {
+    const location = useLocation();
+    const [isConnected, setIsConnected] = useState(false);
+
+    useEffect(() => {
+        location.pathname === '/user'
+            ? setIsConnected(true)
+            : setIsConnected(false);
+    }, [location]);
+
+    return (
+        <header className={styles.mainNav}>
+            <Link to="/" className={styles.mainNavLogo}>
+                <img
+                    src={logo}
+                    className={styles.mainNavLogoImage}
+                    alt="Logo ArgentBank"
+                />
+            </Link>
+            {isConnected ? (
+                <div>
+                    <Link to="/signIn" className={styles.mainNavItem}>
+                        <i className="fa fa-user-circle"></i> Tony&nbsp;
+                    </Link>
+                    <Link to="/" className={styles.mainNavItem}>
+                        <i className="fa fa-sign-out"></i> Sign Out
+                    </Link>
+                </div>
+            ) : (
+                <Link to="/signIn" className={styles.mainNavItem}>
+                    <i className="fa fa-user-circle"></i> Sign In
+                </Link>
+            )}
+        </header>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+/* COMPONENTS */
+import Header from '@components/Header';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<></>} />
             </Routes>
