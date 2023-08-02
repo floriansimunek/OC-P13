44a293804f021a971a4615c94137862305c3be89
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-02 23:26:36 +0200

    :sparkles: Add mocked data

---
+++ src/mocks/UsersData.js
@@ -0,0 +1,14 @@
+export const USERS = [
+    {
+        id: 1,
+        email: 'tony@stark.com',
+        firstName: 'Tony',
+        lastName: 'Stark',
+    },
+    {
+        id: 2,
+        email: 'steve@rogers.com',
+        firstName: 'Steve',
+        lastName: 'Rogers',
+    },
+];
