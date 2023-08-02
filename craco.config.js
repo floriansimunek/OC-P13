3c404779b9bd917376d70737f8a33611a031e2be
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-02 23:23:10 +0200

    :wrench: Add mocks folder alias

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,7 @@ module.exports = {
             '@assets': path.resolve(__dirname, 'src/assets'),
             '@components': path.resolve(__dirname, 'src/components'),
             '@pages': path.resolve(__dirname, 'src/pages'),
+            '@mocks': path.resolve(__dirname, 'src/mocks'),
         },
     },
 };
