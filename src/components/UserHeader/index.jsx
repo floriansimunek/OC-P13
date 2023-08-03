7a06ffa10fba498a9d62d9cc8841c5903fb36499
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-03 19:24:12 +0200

    :sparkles: Add modal

--- src/components/UserHeader/index.jsx
+++ src/components/UserHeader/index.jsx
@@ -1,7 +1,16 @@
+/* IMPORTS */
+import { useState } from 'react';
+
 /* CSS */
 import styles from './UserHeader.module.scss';
 
 export default function UserHeader({ user }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleToggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
     return (
         <header className={styles.userHeader}>
             <h1>
@@ -9,7 +18,37 @@ export default function UserHeader({ user }) {
                 <br />
                 {`${user.firstName} ${user.lastName}`}
             </h1>
-            <button className={styles.editButton}>Edit Name</button>
+            <button className={styles.editButton} onClick={handleToggleModal}>
+                Edit Name
+            </button>
+            {isModalOpen && (
+                <div className={styles.modal}>
+                    <div className={`${styles.modalContent} bgDark`}>
+                        <div
+                            className={styles.modalClose}
+                            onClick={handleToggleModal}
+                        >
+                            &#10060;
+                        </div>
+                        <form>
+                            <div className={styles.inputWrapper}>
+                                <label htmlFor="firstname">Prénom</label>
+                                <input type="text" id="firstname" />
+                            </div>
+                            <div className={styles.inputWrapper}>
+                                <label htmlFor="lastname">Nom</label>
+                                <input type="text" id="lastname" />
+                            </div>
+                            <button className={styles.modalButton}>
+                                Mettre à jours mes données
+                            </button>
+                        </form>
+                        {/* {userLoginError && (
+                            <p className={styles.error}>{userLoginError}</p>
+                        )} */}
+                    </div>
+                </div>
+            )}
         </header>
     );
 }
