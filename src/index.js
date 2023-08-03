e6da60184c817a1407eb97296074f1e957ff9569
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-03 14:27:02 +0200

    :sparkles: Add store Provider

--- src/index.js
+++ src/index.js
@@ -15,17 +15,23 @@ import User from '@pages/User';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
+/* REDUX */
+import { Provider } from 'react-redux';
+import store from '@store/index';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signIn" element={<SignIn />} />
-                <Route path="/user/:id" element={<User />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signIn" element={<SignIn />} />
+                    <Route path="/user/:id" element={<User />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </React.StrictMode>
+    </Provider>
 );
