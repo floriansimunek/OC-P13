c26a2670d564cf9be2ad4f4a913f58ca707f4ce7
Florian SIMUNEK <florian.simunek@gmail.com>
2023-08-03 14:15:13 +0200

    :sparkles: Add login method

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,8 +1,8 @@
 export default class Service {
     static baseUrl = '';
 
-    static async fetchData(path) {
-        const response = await fetch(`${this.baseUrl}${path}`);
+    static async fetchData(path, options) {
+        const response = await fetch(`${this.baseUrl}${path}`, options);
         const data = await response.json();
         return data;
     }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,29 @@
+/* IMPORTS */
 import MockedService from './MockedService';
+import Service from './Service';
 import User from '@entities/User';
 
-export default class UserService extends MockedService {
+export default class UserService extends Service {
+    static baseUrl = 'http://localhost:3001/api/v1/user';
+
     static async getUserData(userId) {
         const data = await this.fetchData(`/${userId}`);
         const user = new User(data);
         return user;
+    }
+
+    static async login(email, password) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+        };
+
+        try {
+            const response = await this.fetchData('/login', requestOptions);
+            return response.data;
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 }
